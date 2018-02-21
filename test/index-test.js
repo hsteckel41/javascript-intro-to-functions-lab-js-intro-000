521789b53b3cf9fb4eb5f62244bc936433868142
henrysteckel@gmail.com <henrysteckel@gmail.com>
2018-02-21 03:30:09 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -44,6 +44,9 @@ describe('logWhisper(string)', function() {
     console.log.restore()
   })
 })
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
 
 describe('sayHiToGrandma(string)', function() {
   it('returns "I can\'t hear you!" if `string` is lowercase', function() {
