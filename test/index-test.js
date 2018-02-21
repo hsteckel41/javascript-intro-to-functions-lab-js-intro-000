44a57655254a63eeb543978a8e37abcfc961f48e
henrysteckel@gmail.com <henrysteckel@gmail.com>
2018-02-21 03:27:46 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -30,7 +30,7 @@ describe('logShout(string)', function() {
   })
 })
 function logShout(string) {
-  console.log(string.toUpperCase)
+  console.log(string.toUpperCase())
 }
 
 describe('logWhisper(string)', function() {
